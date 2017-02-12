c354f62f4b68fa27456d90d5288a0484708247fe
harshitaparmar <2014harshita@gmail.com>
2017-02-12 23:45:30 +0530

    added

--- angular/services/ang-service.js
+++ angular/services/ang-service.js
@@ -1,9 +1,9 @@
 myApp.factory('iceandfireService',['$http','$q',function($http,$q){
 
 	var sampleAPIS  =  {};
-	var getbooks = $http.get('http://www.anapioficeandfire.com/api/books?page=1&pageSize=20');
-    var getcharacters = $http.get('http://www.anapioficeandfire.com/api/characters?page=1&pageSize=20');
-    var gethouses = $http.get('http://www.anapioficeandfire.com/api/houses?page=1&pageSize=20');
+	var getbooks = $http.get('https://www.anapioficeandfire.com/api/books?page=1&pageSize=20');
+    var getcharacters = $http.get('https://www.anapioficeandfire.com/api/characters?page=1&pageSize=20');
+    var gethouses = $http.get('https://www.anapioficeandfire.com/api/houses?page=1&pageSize=20');
        sampleAPIS.getData=function(){
        	 
        	console.log("helo " + $q.all([getbooks,getcharacters,gethouses]));
